80982b2d51878b1b67a5a940424b5be879bb6b87
Don-Burns <donal.w.burns@hotmail.com>
2020-12-17 17:46:52 +0000

    Final commit of data

--- code/data_extraction.py
+++ code/data_extraction.py
@@ -10,11 +10,14 @@ Default = "../data/maildir/".
 ## imports 
 # wrangling
 import pandas as pd
-import numpy as np
 
 # os agnostic directory handling
 from os import walk
 from os.path import relpath, join
+
+# email parsing
+from email.parser import Parser
+import re
 
 # command line arguments
 from sys import argv
@@ -53,6 +56,48 @@ def extract(root_dir):
     error_output = pd.DataFrame({"Path" : error_paths, "Content" : error_contents})
     return [output, error_output]
 
+
+def split_emails(emails):
+    """
+    takes emails as strings and splits them into From, To, Subject and Body
+
+    Args:
+        emails (list): list of emails as strings
+
+    Returns:
+        [type]: [description]
+    """
+
+    nrow = emails.shape[0]
+
+    # specify empty lists with explicit length 
+    from_list = [None] * nrow
+    to_list = [None] * nrow
+    X_from_list = [None] * nrow
+    X_to_list = [None] * nrow
+    subject_list = [None] * nrow
+    body_list = [None] * nrow
+
+    # loop and store email elements in above lists
+    for i, content in enumerate(emails["Content"]):
+        email = parse_email(content)
+        from_list[i] = email["From"]
+        to_list [i] = email["To"]
+        X_from_list[i] = email["X-From"]
+        X_to_list[i] = email["X-To"]
+        subject_list [i] = email["Subject"]
+        body = email.get_payload()
+        
+        # cleanup body
+        body = re.sub("\n", "", body) #  remove newline chars
+        body_list [i] = body
+
+    email_contents = pd.DataFrame({"From" : from_list, "To" : to_list,
+                                "X-From" : X_from_list, "X-To" : X_to_list,
+                                "Subject" : subject_list, "Body" : body_list})
+    return email_contents
+
+
 if __name__ == "__main__":  
     # check for command line arguments
     if len(argv) > 1:
@@ -71,3 +116,10 @@ if __name__ == "__main__":
     # console feedback
     print("Files successfully extracted: %d" % len(output["Path"]))
     print("Files unsuccessfully extracted: %d" % len(error_output["Path"]) )
+
+    #split emails
+    print("Splitting emails into respective fields and saving output...")
+    email_contents = split_emails(output)
+    email_contents = pd.concat([output["Path"], email_contents], axis=1)
+    email_contents.to_csv(relpath("../data/email_fields.csv"))
+    print("Done")
